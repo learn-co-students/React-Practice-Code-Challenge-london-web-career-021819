00b243c94e1a15a10a1e706e4598f77ebe0f862e
mariamjaludi <mariam.jaludi@gmail.com>
2019-04-30 13:53:52 +0100

    Done.

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,49 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
+
+import Sushi from '../components/Sushi'
 import MoreButton from '../components/MoreButton'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
-}
+const URL= 'http://localhost:3000/sushis'
 
-export default SushiContainer
+export default class SushiContainer extends Component{
+
+  state = {
+    sushis: [],
+    sushiPosition: 0
+  }
+
+componentDidMount(){
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(sushis => this.setState({sushis}))
+  }
+
+  filterSushi = () => {
+    let sushiPosition = this.state.sushiPosition
+    let nextPosition = sushiPosition + 4
+    return this.state.sushis.slice(sushiPosition, nextPosition )
+  }
+
+  onButtonClick = () => {
+    if (this.state.sushiPosition < 95) {
+      let nextPosition = this.state.sushiPosition + 4
+      this.setState({sushiPosition: nextPosition})
+    } else {
+      this.setState({sushiPosition: 0})
+    }
+  }
+
+
+  render (){
+    return (
+      <Fragment>
+        <div className="belt">
+          {
+            this.filterSushi().map(sushi => <Sushi sushi={sushi} key={sushi.id} amIBroke={this.props.amIBroke} eatenSushis={this.props.eatenSushis}/>)
+          }
+          <MoreButton onButtonClick={this.onButtonClick} />
+        </div>
+      </Fragment>
+    )
+  }
+}
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,32 +1,28 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 
-const Table = (props) => {
+export default class Table extends Component {
 
-  const renderPlates = (array) => {
+  renderPlates = (array) => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
+      return <div className="empty-plate" key={x.id} style={{ top: -7 * index }}/>
     })
   }
 
-  return (
-    <Fragment>
-      <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
-      </h1>
-      <div className="table">
-        <div className="stack">
-          {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
-          }
+  render(){
+    const {eatenSushis} = this.props
+    return (
+      <Fragment>
+        <h1 className="remaining">
+          You have: ${this.props.wallet} remaining!
+        </h1>
+        <div className="table">
+          <div className="stack">
+            {
+              this.renderPlates(eatenSushis)
+            }
+          </div>
         </div>
-      </div>
-    </Fragment>
-  )
+      </Fragment>
+    )
+  }
 }
-
-export default Table
